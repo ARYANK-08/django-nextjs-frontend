6fea4ce0996d607831a9e6fdc8f9bc72ec39a036
ARYANK-08 <kyathamaryan@gmail.com>
2024-11-04 03:09:16 +0530

    BASE LAYOUT classname error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
     extends: [
+       'next', 
+       'next/core-web-vitals',
       "plugin:react/recommended",
       "plugin:@typescript-eslint/recommended",
     ],
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,9 +50,9 @@ export default function RootLayout({
    
 
         <AuthProvider> 
-          <BaseLayout>
-          {children}
-          </BaseLayout>
+        <BaseLayout className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col bg-muted/40">
+                {children}
+            </BaseLayout>
           </AuthProvider>
         </ThemeProvider>
     </Suspense>
